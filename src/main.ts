43fb379851c457f6fa843b12cbefb71ca8af4e3d
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2022-11-16 16:37:47 +0100

    commentaire de main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
+// importations de modules principaux
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// importations de modules secondaires
+
+// feuilles de style
 import './assets/main.css'
 
+// création de l'application
 const app = createApp(App)
 
+// application des modules
 app.use(router)
 
+// montage de l'application
 app.mount('#app')
