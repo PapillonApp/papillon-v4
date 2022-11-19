6d19201a68fbeaa83e8c9d6aad8f79707b0cbb07
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2022-11-19 15:20:35 +0100

    ✔ fixed calendar looking weird on ripple

--- src/components/main/TabName.vue
+++ src/components/main/TabName.vue
@@ -177,6 +177,7 @@
     #CalendarLabel {
         color: var(--brand-color);
         border-radius: 5px;
+        margin-right: 10px;
     }
 
     #Calendar {
@@ -192,7 +193,6 @@
 
         width: fit-content;
         padding-right: 0px !important;
-        margin-right: 10px;
     }
 
     #Calendar * {
