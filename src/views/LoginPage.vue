84fe0f4ae608177a806ec57b485b897eb64c3c88
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2022-11-30 18:38:55 +0100

    Fix de l'ancien login avec la nouvelle api GraphQL

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -42,13 +42,23 @@
                 }
 
                 // send login request
-                axios.get(constructAuthURL(loginData))
-                    .then((response) => {
+                fetch(API + "/auth/login", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        username: loginData.username,
+                        password: loginData.password,
+                        url: loginData.url,
+                        cas: loginData.cas,
+                    }),
+                }).then((response) => response.json()).then((data) => {
 
                         // check if token is valid
-                        if(response.data.token != undefined) {
+                        if(data.token != undefined) {
                             // save token
-                            localStorage.setItem('token', response.data.token)
+                            localStorage.setItem('token', data.token)
                             // save credentials
                             localStorage.setItem('loginData', JSON.stringify(loginData))
 
@@ -57,7 +67,7 @@
                         }
                         else {
                             // get error from dictionnary
-                            let error = API_LOGIN_ERRORS[response.data.message]
+                            let error = API_LOGIN_ERRORS[data.message]
                             // show error
                             Toastify({
                                 text: error.message,
