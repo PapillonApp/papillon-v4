1e7b865be7d3aae040081a44c390c145842883a5
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2022-12-06 18:32:14 +0100

    Tentative de rétablissement de l’erreur de Luc’Hack
    (Attention à tester un code avant de le commit ou alors faire une PR)

--- src/views/CoursTab.vue
+++ src/views/CoursTab.vue
@@ -279,9 +279,6 @@
                 // add closestCours = true to the closest cours and false to the others
                 // check if rn is today
                 if(this.currentDate.getDate() == rnToday.getDate()) {
-                    if (closestCours === null) {
-                            break;
-                    }
                     for (let i = 0; i < this.cours.length; i++) {
                         if (this.cours[i].from == closestCours.from) {
                             this.cours[i].closestCours = true
